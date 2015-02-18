cfcd49cc34020cb63718c6c211e2ad793f3818c4
Hackbright Student <info@hackbrightacademy.com>
2015-02-18 14:58:49 -0800

    added test class with user story

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,41 @@
 from selenium import webdriver 
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase): 
+    
+    def setUp(self): 
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self): 
+        self.browser.quit()
+
+    def test_can_search_for_a_recipe(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Recipes', self.browser.title)
+        self.fail('Finish the test!')
+        # User can search for a recipe 
+
+    # User can view search results for 3-10 recipes
+
+    # User can select a recipe and view all ingredients and instructions
+
+    # User can save recipe to recipe book
+
+    # User can filter recipes by:
+        # ingredient
+        # course
+        # cuisine 
+        # holiday
+
+    # User can delete recipes
+
+    # User can add recipes to planner
+
+    # User can create a shopping list from planner
+
+    # User can export shopping list 
+        # via email
+        # via SMS
+
+if __name__ == '__main__':
+    unittest.main()
