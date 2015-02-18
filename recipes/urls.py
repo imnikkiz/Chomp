a04cf918155f856abb00b0d762353b8fe6397416
Hackbright Student <info@hackbrightacademy.com>
2015-02-18 15:26:21 -0800

    Create first unit test, url mapping

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'recipes.views.home', name='home'),
+    url(r'^$', 'search.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
