103a7b07dbf55083aaaaebceb638cdb370f2438d
Hackbright Student <info@hackbrightacademy.com>
2015-02-18 16:13:21 -0800

    Functional test now checks we can input search keyword

--- functional_tests.py
+++ functional_tests.py
@@ -10,12 +10,35 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self): 
         self.browser.quit()
 
+    # User can search for a recipe 
     def test_can_search_for_a_recipe(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('Recipes', self.browser.title)
-        self.fail('Finish the test!')
-        # User can search for a recipe 
+        
+        # User can find a search box
+        header_text = self.browser.find_element_by_tag_name('h1').header_text
+        self.assertIn('Search', header_text)
+        inputbox = self.browser.find_element_by_id('id_search_box')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Search for recipes'
+        )
 
+        # User can search for 'chicken soup'
+        inputbox.send_keys('chicken soup')
+
+        # User can hit enter
+        inputbox.send_keys(Keys.ENTER)
+
+        # The page updates and now provides the search term in a table
+        table = self.browser.find_element_by_id('id_recipe_search_results')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'chicken soup' for row in rows)
+        )
+
+        
+        self.fail('Finish the test!')
     # User can view search results for 3-10 recipes
 
     # User can select a recipe and view all ingredients and instructions
