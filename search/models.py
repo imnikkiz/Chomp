5c54c355264f7612f4f49b5f7853a46ceba2be4d
Hackbright Student <info@hackbrightacademy.com>
2015-02-19 19:00:03 -0800

    Search uses actual search keyword

--- search/models.py
+++ search/models.py
@@ -5,26 +5,34 @@ YUMMLY_APP_ID = os.environ['YUMMLY_APP_ID']
 YUMMLY_APP_KEY = os.environ['YUMMLY_APP_KEY']
 
 def search_yummly_recipes_by_keyword(keyword):
-    keyword.strip().split()
+    print keyword
+
+    keyword = keyword.strip()
+    keyword = keyword.split(' ')
     keyword = '+'.join(keyword)
+
+    print keyword
+
     params = {'_app_id': YUMMLY_APP_ID,
               '_app_key': YUMMLY_APP_KEY,
               'q': keyword}
+
+    print params
+
     recipes_from_yummly = requests.get(
         "http://api.yummly.com/v1/api/recipes",
-        params=params).json()
-    print recipes_from_yummly
+        params=params)
+    print recipes_from_yummly.url
+    recipes_from_yummly = recipes_from_yummly.json()
 
     recipe_list = []
 
     for recipe in recipes_from_yummly['matches']:
-        print recipe
         new_recipe = Recipe()
         new_recipe.name = recipe.get('recipeName')
         new_recipe.yummly_id = recipe.get('id')
         recipe_list.append(new_recipe.name)
 
-    print recipe_list
     return recipe_list
 
 
