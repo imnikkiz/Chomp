ab08ae725fe1a3af6d9b2ccdc86068afd8d3196c
Hackbright Student <info@hackbrightacademy.com>
2015-02-24 09:48:13 -0800

    All 10 API request results stored in db; only first 3 shown

--- search/views.py
+++ search/views.py
@@ -8,19 +8,23 @@ def home_page(request):
 
     if request.method == 'POST':
         keyword = request.POST['search_keyword_text']
-        if Search.objects.filter(keyword=keyword):
-            this_search = Search.objects.get(keyword=keyword)
-            recipe_list = this_search.recipes.all()
-        else:
+
+        # Keyword not in database
+        if not Search.objects.filter(keyword=keyword):
             new_search = Search.objects.create()
             new_search.search_by_keyword(keyword=keyword)
 
-            for recipe in new_search.response['matches'][0:3]:
+            for recipe in new_search.response['matches']:
                 new_recipe = Recipe.objects.create()
                 new_recipe.get_recipe_by_yummly_id(yummly_id=recipe.get('id'))
                 new_search.recipes.add(new_recipe)
-                recipe_list.append(new_recipe)
                 link_ingredient_to_recipe(new_recipe.id)
+
+        this_search = Search.objects.get(keyword=keyword)
+        recipe_list = this_search.recipes.all()[:3]
+
+
+
 
     return render(request, 'home.html', {
         'recipe_list': recipe_list
@@ -43,5 +47,5 @@ def view_recipes(request):
         this_recipe = Recipe.objects.get(id=this_recipe_id)
         # flash('Recipe added!')
         # add to db
-        
+
     return render(request, 'my_recipes.html')
