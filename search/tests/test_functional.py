925a8dc0ad31b4c6345b2b5a6f5d653bc359eaac
Hackbright Student <info@hackbrightacademy.com>
2015-02-25 16:14:43 -0800

    Revise functional test

--- search/tests/test_functional.py
+++ search/tests/test_functional.py
@@ -1,39 +1,74 @@
-from django.test import LiveServerTestCase
+from django.test import LiveServerTestCase, Client
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase): 
+class NewUserTest(LiveServerTestCase): 
     
     def setUp(self): 
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(1)
+
 
     def tearDown(self): 
         self.browser.quit()
 
-    # User can search for a recipe 
-    def test_can_search_for_a_recipe(self):
-        self.browser.get(self.live_server_url)
-        self.assertIn('Recipes', self.browser.title)
-        
-        # User can find a search box
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Search', header_text)
-        inputbox = self.browser.find_element_by_id('id_search_box')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Search for recipes'
-        )
 
+    def test_user_can_register_and_login(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn('Home', self.browser.title)
+
+
+        # new user can register
+        register_link = self.browser.find_element_by_link_text('Register Here')
+        register_link.click()
+        self.assertIn('Register', self.browser.title)
+
+        self.browser.find_element_by_id('id_username').send_keys('Username')
+        self.browser.find_element_by_id('id_email').send_keys('email@email.com')
+        self.browser.find_element_by_id('id_password').send_keys('Password')  
+        register_button = self.browser.find_element_by_id('id_register')
+        register_button.click()
+
+        next_page_text = self.browser.find_element_by_id('content').text
+        self.assertIn('Thank you for registering!', next_page_text)
+
+        home_link = self.browser.find_element_by_link_text('Return to the homepage.')
+        home_link.click()
+
+        self.assertIn('Home', self.browser.title)
+        self.browser.implicitly_wait(1)
+
+
+        # new user can login
+        login_link = self.browser.find_element_by_link_text('Login')
+        login_link.click()
+
+        self.assertIn('Login', self.browser.title)
+
+        self.browser.find_element_by_id('id_username').send_keys('Username')
+        self.browser.find_element_by_id('id_password').send_keys('Password')  
+        submit_button = self.browser.find_element_by_id('id_submit')
+        submit_button.click()
+
+        self.assertIn('Search Recipes', self.browser.title)
+        self.browser.implicitly_wait(1)
+
+        
         # User can search for 'chicken soup'
+        inputbox = self.browser.find_element_by_id('id_search_box')      
         inputbox.send_keys('chicken soup')
         inputbox.send_keys(Keys.ENTER)
 
-        # User can view search results for 3 recipes
-        recipe_results = self.browser.find_element_by_id('id_recipe_search_results').text
-        self.assertIn('Chicken', recipe_results)
+        recipe_results = self.browser.find_elements_by_class_name('col-xs-4')
+        for recipe in recipe_results:
+            self.assertIn('Chicken', recipe.text)
+        self.browser.implicitly_wait(1)
         
+        
+class WriteMoreTests(LiveServerTestCase):
+
+    def tests_succeed(self):
         self.fail('Finish the test!')
 
     # User can select a recipe and view all ingredients and instructions
